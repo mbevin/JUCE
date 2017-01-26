4b6dc603a2c1986682c115cd99a778446dea1ab8
Michael Bevin <mbevin@gmail.com>
2017-01-26 13:12:42 +1300

    Some checks/extra-locking that was playing with - don’t seem to have been the issue, so disabled.

--- modules/juce_graphics/native/juce_RenderingHelpers.h
+++ modules/juce_graphics/native/juce_RenderingHelpers.h
@@ -167,6 +167,8 @@ public:
     void reset()
     {
         const ScopedLock sl (lock);
+        //jassert (MessageManager::getInstance()->currentThreadHasLockedMessageManager());
+        
         glyphs.clear();
         addNewGlyphSlots (120);
         hits.set (0);
@@ -175,6 +177,15 @@ public:
 
     void drawGlyph (RenderTargetType& target, const Font& font, const int glyphNumber, Point<float> pos)
     {
+        //const ScopedLock sl (lock);
+
+		  //
+		  // note - seems we don't have a locked-message-manager when we get here from our code ... however, when
+		  // our code was entirely disabled, we were still getting the crashes in here ... and it looks like its
+		  // the right thread...
+		  //
+        //jassert (MessageManager::getInstance()->currentThreadHasLockedMessageManager());
+        
         if (ReferenceCountedObjectPtr<CachedGlyphType> glyph = findOrCreateGlyph (font, glyphNumber))
         {
             glyph->lastAccessCount = ++accessCounter;
@@ -185,6 +196,7 @@ public:
     ReferenceCountedObjectPtr<CachedGlyphType> findOrCreateGlyph (const Font& font, int glyphNumber)
     {
         const ScopedLock sl (lock);
+        //jassert (MessageManager::getInstance()->currentThreadHasLockedMessageManager());
 
         if (CachedGlyphType* g = findExistingGlyph (font, glyphNumber))
         {
